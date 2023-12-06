c4723f882d8f6a35ac32c59a4f8c0ea31c41eb14
Tyler Tippens <tyftip@gmail.com>
2023-12-06 07:06:04 -0500

    Big changes to make the GPU work. I think it works, but its too slow for the brutest force method

--- 5/day5.cpp
+++ 5/day5.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <cstdio>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <regex>
 #include <limits.h>
+#include <omp.h>
+#include <ctime>
 using namespace std;
 
-long dest_from_src(vector<vector<long>> mapping, long the_id)
+long dest_from_src(long* mapping, size_t num_lines, long the_id)
 {
     long dest, src, span;
 
-    for (const auto& mp : mapping)
+    // iterate by 3 since each lines has 3 numbers
+    for (int mi = 0; mi < num_lines; mi++)
     {  // for each line/range
-        dest = mp[0];
-        src = mp[1];
-        span = mp[2];
+	dest = *(mapping + 3*mi);
+        src = *(mapping + 3*mi + 1);
+        span = *(mapping + 3*mi + 2);
 
         if ((src <= the_id) && (the_id < (src + span)))
         {
@@ -27,23 +31,32 @@ long dest_from_src(vector<vector<long>> mapping, long the_id)
     return the_id;
 }
 
-long get_location(vector<vector<vector<long>>> data, long the_id)
+long get_location(long* data, size_t* map_sizes, size_t mps_len, long the_id)
 {
-    for (const auto& m : data)
+    size_t jump {0};
+    int doffset {0};
+
+    for (int m = 0; m < mps_len; m++)
     { // for each mapping
-        the_id = dest_from_src(m, the_id);
+	// get the destination id from the source id
+	// there are 3 numbers on each line
+	doffset += m ? 3*jump : 0;
+      	jump = *(map_sizes + m);
+	the_id = dest_from_src(data + doffset, jump, the_id);
     }
+
     return the_id;
 }
 
 int main(int argc, char* argv[])
 {
-    long seed_start, seed_span, dist;
+    long seed_start, seed_span;
     long min_dist {LONG_MAX};
 
-    vector<long> line_nums, seeds;
-    vector<vector<long>> map_nums;
-    vector<vector<vector<long>>> data;
+    vector<long> seeds, indata;
+
+    size_t map_lines {0};
+    vector<size_t> map_sizes;
 
     string line{}, seedstr{}, aseed{};
     size_t pos {0};
@@ -53,6 +66,14 @@ int main(int argc, char* argv[])
 
     regex num_ptrn {"\\d+"};
     regex map_ptrn {"map"};
+
+    // sanity check
+    cout << "Devices: " << omp_get_num_devices() << endl;
+
+    omp_set_num_threads(1);
+
+    time_t start_t;
+    time(&start_t);
 
     if (infile.is_open())
     {
@@ -75,42 +96,73 @@ int main(int argc, char* argv[])
             else if (regex_search(line, map_ptrn))
             {
                 // new map tier
-                // store and reset the map number vectors
-                data.push_back(map_nums);
-                map_nums = vector<vector<long>>();
+		if (map_lines)
+		    map_sizes.push_back(map_lines);
+		map_lines = 0;
             }
             else
             {
                 // build data structure:
-                // [ #map [ #range ( destination, source, span ), ... ], ... ]
-                line_nums = vector<long>();
                 while (regex_search(line, match, num_ptrn))
                 {
                     // line has numbers, loop through them and add to the vector
-                    line_nums.push_back(stol(match[0]));
+		    indata.push_back(stol(match[0]));
                     line = match.suffix().str();
                 }
-
-                // add line numbers to this map's vector
-                map_nums.push_back(line_nums);
+		map_lines++;
             }
         }
+
+	// get last map size
+	map_sizes.push_back(map_lines);
     }
 
-    for (int i = 0; i < seeds.size(); i += 2)
+    // we have to prepare the data for sending to the GPU, it doesn't like vectors
+    long* datarr = indata.data();
+    long* seedsarr = seeds.data();
+    size_t* map_sz_arr = map_sizes.data();
+
+    size_t num_maps = map_sizes.size();
+    size_t num_seeds = seeds.size();
+
+    cout << "Input read took " << time(NULL) - start_t << " s" << endl;
+    time(&start_t);
+
+// allocate and send data to the GPU
+#pragma omp target enter data \
+    map(to:seedsarr[:seeds.size()],datarr[:indata.size()],map_sz_arr[:map_sizes.size()])
+
+// on the GPU; min_dist is tofrom so it gets updated on the host
+#pragma omp target map(tofrom:min_dist)
+{
+    // Split the seeds among teams, with a min reduction to combine them
+    //#pragma omp teams num_teams(num_seeds/2) reduction(min : min_dist)
+    #pragma omp teams reduction(min : min_dist)
+	
     {
-        seed_start = seeds[i];
-        seed_span = seeds[i+1];
+	long dist;
+	#pragma omp distribute
+    	for (int i = 0; i < num_seeds; i += 2)
+    	{
+            seed_start = seedsarr[i];
+       	    seed_span = seedsarr[i + 1];
 
-        for (long s = seed_start; s < seed_start + seed_span; s++)
-        {
-            dist = get_location(data, s);
+	    // Split seed exploration among threads, again with a min reduction
+	    #pragma omp parallel for reduction(min : min_dist) private(dist)
+            for (long s = seed_start; s < seed_start + seed_span; s++)
+            {
+                dist = get_location(datarr, map_sz_arr, num_maps, s);
 
-            if (dist < min_dist)
-                min_dist = dist;
-        }
+                if (dist < min_dist)
+                    min_dist = dist;
+            }
+	}
     }
+}
+
+    cout << "Brute forcing took " << time(NULL) - start_t << " s" << endl;
 
     cout << "Part 2: " << min_dist << endl;
 
+    return 0;
 }
